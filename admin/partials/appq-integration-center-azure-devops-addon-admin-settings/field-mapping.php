071f172a006d701c2fb8ab4870ea7da179ab6950
Davide Bizzi <davide.bizzi@app-quality.com>
2019-10-25 11:25:12 +0200

    style: Basic style for field mapping

--- admin/partials/appq-integration-center-azure-devops-addon-admin-settings/field-mapping.php
+++ admin/partials/appq-integration-center-azure-devops-addon-admin-settings/field-mapping.php
@@ -4,9 +4,9 @@
 	<div class="col-sm-9 field_mapping">
 		<?php foreach ($field_mapping as $key => $value) : ?>
 			<div class="form-group row">
-				<label for="field_mapping[<?= $key ?>]" class="col-sm-2"><?= $key ?></label>
+				<label style="word-break: break-all;" for="field_mapping[<?= $key ?>]" class="col-sm-2 align-self-center text-center"><?= $key ?></label>
 				<textarea name="field_mapping[<?= $key ?>]" class="col-sm-9 form-control" placeholder="Title: {Bug.title}"><?= $value ?></textarea>
-				<button class="col-sm-1 remove btn btn-danger">-</button>
+				<button class="col-sm-1 remove btn btn-danger"><span class="fa fa-minus"></span></button>
 			</div>
 		<?php endforeach ?>
 		<button type="button" class="add_field_mapping col-sm-12 btn btn-primary">+</button>
