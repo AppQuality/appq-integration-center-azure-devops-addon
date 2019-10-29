bbb8ee8cfa792069e9108fdfefdafb71509d884b
Davide Bizzi <davide.bizzi@app-quality.com>
2019-10-29 18:03:23 +0100

    feat: Add upload attachment

--- class/class-appq-integration-center-azure-devops-api.php
+++ class/class-appq-integration-center-azure-devops-api.php
@@ -76,13 +76,21 @@ class AzureDevOpsRestApi extends IntegrationCenterRestApi
 
 		if (property_exists($res, 'fields'))
 		{
-			$wpdb->insert($wpdb->prefix . 'appq_integration_center_bugs', array(
-				'bug_id' => $bug->id,
-				'integration' => $this->integration['slug']
-			));
+			// $wpdb->insert($wpdb->prefix . 'appq_integration_center_bugs', array(
+			// 	'bug_id' => $bug->id,
+			// 	'integration' => $this->integration['slug']
+			// ));
+			if (property_exists($this->configuration, 'upload_media') && intval($this->configuration->upload_media) > 0)
+			{
+				$media =  $wpdb->get_col($wpdb->prepare('SELECT location FROM ' . $wpdb->prefix . 'appq_evd_bug_media WHERE bug_id = %d', $bug->id));
+				foreach ($media as $media_item)
+				{
+					$this->add_attachment($bug, $res->id, $media_item);
+				}
+			}
 
 			return array(
-				'status' => true,
+				'status' => false,
 				'message' => $res
 			);
 		}
@@ -90,6 +98,73 @@ class AzureDevOpsRestApi extends IntegrationCenterRestApi
 			'status' => false,
 			'message' => 'Generic error'
 		);
+	}
+	
+	
+
+
+	public function add_attachment($bug, $key, $media)
+	{
+		$basename = basename($media);
+		$filename =  ABSPATH . 'wp-content/plugins/appq-integration-center/tmp/' . $basename;
+		file_put_contents(ABSPATH . 'wp-content/plugins/appq-integration-center/tmp/' . $basename, fopen($media, 'r'));
+
+		$headers = array(
+			"Content-Type: application/octet-stream",
+			"Authorization: " . $this->get_authorization()
+		);
+		
+		$ch = curl_init();
+		$options = array(
+			CURLOPT_URL => $this->get_apiurl(). '/wit/attachments?filename='.$basename.'&api-version=' .$this->api_version,
+			CURLOPT_POST => 1,
+			CURLOPT_HTTPHEADER => $headers,
+			CURLOPT_POSTFIELDS => file_get_contents($filename),
+			CURLOPT_RETURNTRANSFER => true
+		);
+		curl_setopt_array($ch, $options);
+		$req = curl_exec($ch);
+		
+		$ret = array(
+			'status' => false,
+			'message' => 'Generic error on attachment ' . $basename
+		);
+		if(!curl_errno($ch))
+		{
+			$info = curl_getinfo($ch);
+			if ($info['http_code'] == 201) {
+				$req =  json_decode($req);
+				$req = Requests::patch($this->get_apiurl() . '/wit/workitems/' . $key . '?api-version=' . $this->api_version, array(
+					'Authorization' => $this->get_authorization(),
+					'Content-Type' => 'application/json-patch+json'
+				), json_encode(array(
+					array(
+				    	"op" => "add",
+					    "path" => "/relations/-",
+					    "value" => array(
+							"rel" => "AttachedFile",
+							"url" => $req->url,
+						)
+					)
+				)));
+				
+				$ret = array(
+					'status' => true,
+					'message' => json_decode($req->body)
+				);
+			} else {
+				$ret['message'] = $ret['message'] . ' - Error ' .  $info['http_code'];
+			}
+		}
+		else
+		{
+			$ret = array(
+				'status' => false,
+				'error' => $errmsg
+			);
+		}
+		curl_close($ch);
+		unlink($filename);
 	}
 
 	public function get_issue_by_id($id)
