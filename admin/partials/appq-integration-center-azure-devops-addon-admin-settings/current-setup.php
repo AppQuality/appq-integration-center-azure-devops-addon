8b160436784e6b46129f32f5a362e81e30ccd6df
Luca Cannarozzo <cannarozzoluca@live.it>
2022-02-02 20:13:27 +0100

    fix integration center modal and current setting partial

--- admin/partials/appq-integration-center-azure-devops-addon-admin-settings/current-setup.php
+++ admin/partials/appq-integration-center-azure-devops-addon-admin-settings/current-setup.php
@@ -1,53 +1,45 @@
 <?php
-$endpoint = isset( json_decode( $campaign->bugtracker->endpoint )->endpoint ) ? json_decode( $campaign->bugtracker->endpoint )->endpoint : $campaign->bugtracker->endpoint;
-$project  = isset( json_decode( $campaign->bugtracker->endpoint )->project ) ? json_decode( $campaign->bugtracker->endpoint )->project : false;
+$endpoint = isset(json_decode($campaign->bugtracker->endpoint)->endpoint) ? json_decode($campaign->bugtracker->endpoint)->endpoint : $campaign->bugtracker->endpoint;
+$project  = isset(json_decode($campaign->bugtracker->endpoint)->project) ? json_decode($campaign->bugtracker->endpoint)->project : false;
 
 ?>
-<div class="row d-flex">
-    <div class="col-1 d-flex-vertical-center">
-		<?php
-		printf(
-			'<img src="%s" alt="%s">',
-			APPQ_INTEGRATION_CENTER_AZURE_DEVOPS_URL . 'admin/images/icon.svg',
-			$campaign->bugtracker->integration
-		);
-		?>
+<div class="row prevent-cols-breaking">
+    <div class="col-lg-1 align-items-center justify-content-center hidden-xs visible-xl">
+        <?php
+        printf(
+            '<img src="%s" alt="%s" class="img-responsive" style="margin:auto">',
+            APPQ_INTEGRATION_CENTER_AZURE_DEVOPS_URL . 'admin/images/icon.svg',
+            $campaign->bugtracker->integration
+        );
+        ?>
     </div>
-    <div class="col-3">
-        <div>
-			<?php
-			printf( '<h6 class="text-secondary">%s</h6>', __( 'Endpoint', 'appq-integration-center-azure-devops-addon' ) );
-			?>
-            <span class="text-primary"><?= $endpoint ?></span>
-        </div>
+    <div class="col-sm-6 col-lg-3 col-xl-3 align-items-center justify-content-center text-center">
+        <h6 class="text-primary"><?= __('Endpoint', 'appq-integration-center-azure-devops-addon') ?></h6>
+        <span title="<?= $endpoint ?>" class="text-info text-truncate full-width"><?= $endpoint ?></span>
     </div>
-    <div class="col-3">
-        <div>
-			<?php $apikey = $campaign->bugtracker->apikey; ?>
-            <h6 class="text-secondary">
-				<?= __( 'Authentication', 'appq-integration-center-azure-devops-addon' ) ?>
-            </h6>
-            <span>
-                <span class="text-primary"><?= substr( $apikey, 0, 10 ) . str_repeat( "•", 10 ); ?></span>
-                <button data-toggle="modal" data-target="#apiKeyModal" type="button" class="btn btn-link btn-sm pt-0 pb-0">
-                    <i class="fa fa-eye"></i>
-                </button>
-            </span>
-        </div>
+    <div class="col-sm-6 col-lg-3 align-items-center justify-content-center">
+        <?php $apikey = $campaign->bugtracker->apikey; ?>
+        <h6 class="text-primary">
+            <?= __('Authentication', 'appq-integration-center-azure-devops-addon') ?>
+        </h6>
+        <span>
+            <span class="text-info"><?= substr($apikey, 0, 10) . str_repeat("•", 10); ?></span>
+            <button data-toggle="modal" data-target="#apiKeyModal" type="button" class="btn btn-default btn-sm pt-0 pb-0">
+                <i class="fa fa-eye"></i>
+            </button>
+        </span>
     </div>
-    <div class="col">
-        <div>
-			<?php
-			printf( '<h6 class="text-secondary">%s</h6>', __( 'Media upload', 'appq-integration-center-azure-devops-addon' ) );
-			$madiaUploadText = isset( $campaign->bugtracker->upload_media ) && $campaign->bugtracker->upload_media == 1 ? _x( 'Yes', 'allow media upload', 'appq-integration-center-azure-devops-addon' ) : _x( 'No', 'allow media upload', 'appq-integration-center-azure-devops-addon' );
-			?>
-            <span class="text-primary"><?= $madiaUploadText ?></span>
-        </div>
+    <div class="col-sm-6 col-md-3 col-lg-2 align-items-center justify-content-center">
+        <h6 class="text-primary"><?= __('Media upload', 'appq-integration-center-azure-devops-addon') ?></h6>
+        <?php
+        $madiaUploadText = isset($campaign->bugtracker->upload_media) && $campaign->bugtracker->upload_media == 1 ? __('Yes', 'appq-integration-center-azure-devops-addon') : __('No', 'appq-integration-center-azure-devops-addon');
+        ?>
+        <span class="text-info"><?= $madiaUploadText ?></span>
     </div>
-    <div class="col d-flex-vertical-center justify-content-end">
-  			<?php
+    <div class="col-sm-6 col-md-3 col-lg-4 col-xl-3 align-items-center justify-content-center">
+        <?php
         $admin = new AppQ_Integration_Center_Admin('appq-integration-center', APPQ_INTEGRATION_CENTERVERSION);
-        $admin->current_setup_edit_buttons($campaign) 
+        $admin->current_setup_edit_buttons($campaign)
         ?>
     </div>
 </div>
--- admin/partials/appq-integration-center-azure-devops-addon-admin-tracker-settings.php
+++ admin/partials/appq-integration-center-azure-devops-addon-admin-tracker-settings.php
@@ -15,14 +15,14 @@
 <form id="azure-devops_tracker_settings">
     <div class="form-group mt-5">
         <?php
-        printf('<label for="azure_devops_endpoint">%s</label>', __('Endpoint', 'appq-integration-center-azure-devops-addon'));
         printf('<input type="text" class="form-control" name="azure_devops_endpoint" id="azure_devops_endpoint" placeholder="%s" value="%s">', __('https://yourcompanyname.atlassian.com', $this->plugin_name), !empty($config) ? $config->endpoint : '');
+        printf('<label for="azure_devops_endpoint">%s</label>', __('Endpoint', 'appq-integration-center-azure-devops-addon'));
         ?>
     </div>
     <div class="form-group">
         <?php
-        printf('<label for="azure_devops_apikey">%s</label>', __('Authentication', 'appq-integration-center-azure-devops-addon'));
         printf('<input type="text" class="form-control" name="azure_devops_apikey" id="azure_devops_apikey" placeholder="%s" value="%s">', __('PERSONAL ACCESS TOKEN', $this->plugin_name), !empty($config) ? $config->apikey : '');
+        printf('<label for="azure_devops_apikey">%s</label>', __('Authentication', 'appq-integration-center-azure-devops-addon'));
         ?>
     </div>
 </form>
